5702879777955545daaa66d9c36920f78de0da86
ArmandoOnTheGo <armando.mendoza1129@gmail.com>
2020-06-04 20:06:33 -0500

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,11 @@
 package main 	//Package Name
 
-import (
-	"fmt"	//If you need more than one import, you'll need ()
-	"example.com/user/hello/morestrings"	
+import (	//If you need more than one import, you'll need ()
+	"fmt"	
+	"example.com/user/hello/morestrings"	//Importing from another package
 )
 func main() {
-	s := "Hello World"
-	fmt.Printf(s+"\n")
-	//Using the reverse string from morestrigns package
-	fmt.Println(morestrings.ReverseRunes(s))
+	s := "Hello World"				//setting s to String "Hello World"
+	fmt.Printf(s+"\n")				//outputting s NOTE: similar to java
+	fmt.Println(morestrings.ReverseRunes(s))	//Using the reverse string from morestrigns package
 }
