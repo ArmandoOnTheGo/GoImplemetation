a22211c5a74c5db517601f420f0a93249e6641f7
ArmandoOnTheGo <armando.mendoza1129@gmail.com>
2020-06-04 20:05:05 -0500

    Update reverse.go

--- hello/morestrings/reverse.go
+++ hello/morestrings/reverse.go
@@ -3,8 +3,8 @@ package morestrings //Package Name
 func ReverseRunes (s string) string {
 	r := []rune(s)
 	//starts by instanstianting i to 0 and j to len(r) 
-	//then the condition to stay in the for loop "i < len(r)"
-	//then i and j are equal to i + 1 and j - 1 each iteration
+	//then the condition to stay in the for loop "i < len(r)/2"
+	//then i and j are equal to i + 1 and j - 1 each iteration approaching the middle
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
